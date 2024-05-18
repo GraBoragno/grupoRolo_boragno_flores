1a2837a80b56c2cfcec0f027b02626c28e7c4e0e
GraBoragno <graboragno2005@gmail.com>
2024-05-18 17:20:05 -0300

    MyList

---
+++ MyList.java
@@ -0,0 +1,13 @@
+public interface MyList<T> {
+
+    void add(T value);
+
+    T get(int position) throws PosicionInvalida;
+
+    boolean contains(T value);
+
+    void remove(T value) throws EmptyListException, InformacionInvalida;
+
+    int size();
+
+}
