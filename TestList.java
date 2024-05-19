73c993e9122cf28ca1cef0c96a14852a640f0600
GraBoragno <graboragno2005@gmail.com>
2024-05-19 15:14:24 -0300

    Test List

--- TestList.java
+++ TestList.java
@@ -1,14 +1,10 @@
 
 import Exceptions.EmptyListException;
 import Exceptions.InformacionInvalida;
-
 import java.util.EmptyStackException;
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 public class TestList {
-
-    // --------------------- Test List ----------------------
 
     @Test
     public void testListAdd1() throws InformacionInvalida {
@@ -111,51 +107,7 @@ public class TestList {
         assertFalse(prueba.contains(3));
     }
 
-    // --------------- Test Satck ---------------------------
 
-    @Test
-    public void testStackPush1() throws InformacionInvalida{
-        MyStack<Integer> prueba = new LinkedListImpl<>();
-        prueba.push(2);
-        prueba.push(5);
-        prueba.push(23);
-        assertEquals(3, prueba.size());
-    }
-    @Test
-    public void testStackPush2() throws InformacionInvalida{
-        MyStack<Integer> prueba = new LinkedListImpl<>();
-        assertThrows(InformacionInvalida.class, () -> {
-            prueba.push(null);
-        });
-    }
-    @Test
-    public void testStackPop1() throws EmptyStackException, InformacionInvalida{
-        MyStack<Integer> prueba = new LinkedListImpl<>();
-        prueba.push(2);
-        prueba.push(5);
-        prueba.push(7);
-        assertEquals(7, prueba.pop());
-    }
-    @Test
-    public void testStackPop2() throws InformacionInvalida, EmptyStackException {
-        MyStack<Integer> prueba = new LinkedListImpl<>();
-        assertThrows(EmptyStackException.class, () -> {
-            prueba.pop();
-        });
-    }
-    @Test
-    public void testStackTop1() throws InformacionInvalida {
-        MyStack<Integer> prueba = new LinkedListImpl<>();
-        prueba.push(2);
-        prueba.push(5);
-        prueba.push(23);
-        assertEquals(23, prueba.top());
-    }
-    @Test
-    public void testStackTop2() throws InformacionInvalida {
-        MyStack<Integer> prueba = new LinkedListImpl<>();
-        assertNull(prueba.top());
-    }
 
 //    //------------- Test queue ----------------------------
 //    @org.junit.jupiter.api.Test
