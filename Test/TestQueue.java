487a23f455ba12eba869f3a1d813e05ee8db133d
GraBoragno <graboragno2005@gmail.com>
2024-05-20 13:26:23 -0300

    Test Queue

--- Test/TestQueue.java
+++ Test/TestQueue.java
@@ -1,14 +1,21 @@
 package Test;
 
+import Exceptions.EmptyListException;
+import Exceptions.EmptyQueueException;
 import Exceptions.InformacionInvalida;
+import Exceptions.PosicionInvalida;
+import TADS.LinkedListImpl;
+import TADS.MyList;
+import TADS.MyQueue;
+import TADS.MyStack;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestQueue {
     //------------- Test queue ----------------------------
     @org.junit.jupiter.api.Test
-    public void test4() throws InformacionInvalida {
+    public void testContains1() throws InformacionInvalida {
         TADS.MyQueue<Integer> prueba = new TADS.LinkedListImpl<>();
         prueba.enqueue(2);
         prueba.enqueue(33);
@@ -16,7 +23,7 @@ public class TestQueue {
         assertTrue(prueba.contains(33));
     }
     @org.junit.jupiter.api.Test
-    public void test5() throws InformacionInvalida {
+    public void testSize1() throws InformacionInvalida {
         TADS.MyQueue<Integer> prueba = new TADS.LinkedListImpl<>();
         prueba.enqueue(2);
         prueba.enqueue(33);
@@ -24,11 +31,23 @@ public class TestQueue {
         assertEquals(3,prueba.size());
     }
     @org.junit.jupiter.api.Test
-    public void test6() throws Exceptions.EmptyQueueException, InformacionInvalida {
+    public void testDequeue1() throws Exceptions.EmptyQueueException, InformacionInvalida {
         TADS.MyQueue<Integer> prueba = new TADS.LinkedListImpl<>();
         prueba.enqueue(2);
         prueba.enqueue(33);
         prueba.enqueue(754);
         assertEquals(2,prueba.dequeue());
+    }
+    @Test
+    public void testDequeue() throws InformacionInvalida, EmptyQueueException{
+        MyQueue<Integer> prueba = new LinkedListImpl<>();
+        assertThrows(EmptyQueueException.class, prueba::dequeue);
+    }
+    @Test
+    public void testEnqueue1() throws InformacionInvalida{
+        MyQueue<Integer> prueba = new LinkedListImpl<>();
+        assertThrows(InformacionInvalida.class, () -> {
+            prueba.enqueue(null);
+        });
     }
 }
