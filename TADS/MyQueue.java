693c5454a34dd67e8227d03fcdaae4329af6e02e
GraBoragno <graboragno2005@gmail.com>
2024-05-19 22:05:34 -0300

    MyQueue

---
+++ TADS/MyQueue.java
@@ -0,0 +1,14 @@
+package TADS;
+
+import Exceptions.EmptyQueueException;
+import Exceptions.InformacionInvalida;
+
+public interface MyQueue<T> {
+
+    void enqueue(T value) throws InformacionInvalida;
+
+    T dequeue() throws EmptyQueueException;
+
+    boolean contains(T value);
+
+}
