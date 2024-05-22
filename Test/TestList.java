b9ae46f1ee4e9e460163bde8a7c7d65c2a473eb0
GraBoragno <graboragno2005@gmail.com>
2024-05-22 15:04:26 -0300

    test list

--- Test/TestList.java
+++ Test/TestList.java
@@ -9,105 +9,56 @@ import TADS.MyList;
 import org.junit.jupiter.api.Test;
 
 public class TestList {
-
     @Test
-    public void testListAdd1() throws InformacionInvalida {
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        assertThrows(InformacionInvalida.class, () -> {
-            prueba.add(null);
-        });
+    public void testAdd1() {
+        MyList<Integer> list = new LinkedListImpl<>();
+        assertThrows(InformacionInvalida.class, () -> list.add(null));
     }
 
     @Test
-    public void testListGet1() throws PosicionInvalida, InformacionInvalida{
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        prueba.add(56);
-        prueba.add(101);
-        prueba.add(45);
-        assertEquals(101, prueba.get(1));
+    public void testAddyGet2() throws InformacionInvalida, PosicionInvalida {
+        MyList<Integer> list = new LinkedListImpl<>();
+        list.add(357);
+        assertEquals(357, list.get(0));
     }
     @Test
-    public void testListGet2() throws PosicionInvalida {
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        assertThrows(PosicionInvalida.class, () -> {
-            prueba.get(-1);
-        });
+    public void testGet3() {
+        MyList<Integer> list = new LinkedListImpl<>();
+        assertThrows(PosicionInvalida.class, () -> list.get(-5));
+        assertThrows(PosicionInvalida.class, () -> list.get(3));
+    }
+
+    @Test
+    public void testContains1() throws InformacionInvalida {
+        MyList<Integer> list = new LinkedListImpl<>();
+        list.add(16);
+        assertTrue(list.contains(16));
+        assertFalse(list.contains(865));
+    }
+
+    @Test
+    public void testRemove1() throws InformacionInvalida, EmptyListException {
+        MyList<Integer> list = new LinkedListImpl<>();
+        list.add(63);
+        list.remove(63);
+        assertFalse(list.contains(63));
     }
     @Test
-    public void testListGet3() throws PosicionInvalida {
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        assertThrows(PosicionInvalida.class, () -> {
-            prueba.get(5);
-        });
+    public void testRemove2() {
+        MyList<Integer> list = new LinkedListImpl<>();
+        assertThrows(EmptyListException.class, () -> list.remove(1));
     }
     @Test
-    public void testListSize1() throws InformacionInvalida{
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        prueba.add(56);
-        prueba.add(101);
-        prueba.add(45);
-        assertEquals(3, prueba.size());
+    public void testRemove3() throws InformacionInvalida {
+        MyList<Integer> list = new LinkedListImpl<>();
+        list.add(145);
+        assertThrows(InformacionInvalida.class, () -> list.remove(2));
     }
     @Test
-    public void testListSize2(){
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        assertEquals(0, prueba.size());
-    }
-    @Test
-    public void testListRemove1() throws InformacionInvalida, EmptyListException {
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        prueba.add(56);
-        prueba.add(101);
-        prueba.remove(56);
-        assertEquals(1, prueba.size());
-    }
-    @Test
-    public void testListRemove2() throws InformacionInvalida, EmptyListException {
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        prueba.add(56);
-        prueba.add(101);
-        assertThrows(InformacionInvalida.class, () -> {
-            prueba.remove(2);
-        });
-    }
-    @Test
-    public void testListRemove3() throws InformacionInvalida, PosicionInvalida {
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        assertThrows(EmptyListException.class, () -> {
-            prueba.remove(5);
-        });
-    }
-    @Test
-    public void testListRemove4() throws InformacionInvalida, EmptyListException {
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        prueba.add(56);
-        prueba.add(101);
-        prueba.remove(101);
-        assertEquals(1, prueba.size());
-    }
-    @Test
-    public void testListRemove5() throws InformacionInvalida, EmptyListException {
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        prueba.add(56);
-        prueba.add(101);
-        prueba.add(5);
-        prueba.remove(101);
-        assertEquals(2, prueba.size());
-    }
-    @Test
-    public void testListContins1() throws InformacionInvalida{
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        prueba.add(56);
-        prueba.add(101);
-        prueba.add(45);
-        assertTrue( prueba.contains(101));
-    }
-    @Test
-    public void testListContains2() throws InformacionInvalida{
-        MyList<Integer> prueba = new LinkedListImpl<>();
-        prueba.add(56);
-        prueba.add(101);
-        prueba.add(45);
-        assertFalse(prueba.contains(3));
+    public void testSize1() throws InformacionInvalida, EmptyListException {
+        MyList<Integer> list = new LinkedListImpl<>();
+        list.add(63);
+        list.add(56);
+        assertEquals(2,list.size());
     }
 }
